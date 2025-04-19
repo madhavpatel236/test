685772378dd3c3c6c332a2a551f4e59796e3af3e
Madhav Karavadiya <madhavpatel236@gmail.com>
2025-04-19 19:14:32 +0530

    .

--- application/views/AdminHome.php
+++ application/views/AdminHome.php
@@ -35,42 +35,119 @@ if ($userEmail &&  $userRole == 'admin') {
 
 <body>
     <?php $this->load->view('Navbar.php') ?>
-    <!-- 
-    <form method="post" action="<?php print site_url('AuthController/view'); ?>">
-        <button type="submit" class="logout_btn" name="logout_btn"> Logout </button>
-    </form> -->
 
-    <!-- 
-    <div href="#" class="btn  btn-flex btn-light btn-active-primary fw-bolder" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-        <span class="svg-icon svg-icon-5 svg-icon-gray-500 me-1">
-        </span> Logout
-    </div> -->
-
-    <!-- <div>
-        <span style="margin-right: 100px;">Users Number </span>
-        <span>Points </span>
-    </div>
 
     <div>
         <input name="user_number0" id="user_number0" type="number" min="0" />
         <input name="points0" id="points0" type="number" min="0" />
         <input id="edit_id" type="hidden" />
         <button id="plus_btn" name="plus_btn"> + </button>
-    </div> <br> -->
+    </div> <br>
 
     <!-- <div class="plus_data_div" id="plus_data_div" name="plus_data_div"></div> -->
 
     <!-- <button id="add_btn" class="add_btn" name="add_btn"> Add </button> -->
     <!-- <button style="display: none;" id="update_btn" class="update_btn" name="update_btn"> Update </button> -->
+    <div class="card-px text-center pt-20 pb-5">
+        <a href="#" class="btn btn-primary er fs-6 px-8 py-4" data-bs-toggle="modal" data-bs-target="#kt_modal_new_address">Add New Rules</a>
+    </div>
+    <div class="modal fade" id="kt_modal_new_address" tabindex="-1" aria-hidden="true">
+        <!--begin::Modal dialog-->
+        <div class="modal-dialog modal-dialog-centered mw-650px">
+            <!--begin::Modal content-->
+            <div class="modal-content">
+                <!--begin::Form-->
+                <form class="form" action="#" id="kt_modal_new_address_form">
+                    <!--begin::Modal header-->
+                    <div class="modal-header" id="kt_modal_new_address_header">
+                        <!--begin::Modal title-->
+                        <h2>Add New Address</h2>
+                        <!--end::Modal title-->
+                        <!--begin::Close-->
+                        <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
+                            <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+                            <span class="svg-icon svg-icon-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
+                                    <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
+                                </svg>
+                            </span>
+                            <!--end::Svg Icon-->
+                        </div>
+                        <!--end::Close-->
+                    </div>
+                    <!--end::Modal header-->
+                    <!--begin::Modal body-->
+                    <div class="modal-body py-10 px-lg-17">
+                        <!--begin::Scroll-->
+                        <div class="scroll-y me-n7 pe-7" id="kt_modal_new_address_scroll" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-max-height="auto" data-kt-scroll-dependencies="#kt_modal_new_address_header" data-kt-scroll-wrappers="#kt_modal_new_address_scroll" data-kt-scroll-offset="300px">
+                            <!--begin::Input group-->
+                            <div class="row mb-5">
+                                <!--begin::Col-->
+                                <div class="col-md-6 fv-row">
+                                    <!--begin::Label-->
+                                    <label class="required fs-5 fw-bold mb-2">Number of Players</label>
+                                    <!--end::Label-->
+                                    <!--begin::Input-->
+                                    <input name="user_number0" id="user_number0" type="number" min="0" class="form-control form-control-solid" placeholder="Enter Number of players" />
+                                    <!--end::Input-->
+                                </div>
+                                <!--end::Col-->
+                                <!--begin::Col-->
+                                <div class="col-md-6 fv-row">
+                                    <!--end::Label-->
+                                    <label class="required fs-5 fw-bold mb-2">Points</label>
+                                    <!--end::Label-->
+                                    <!--end::Input-->
+                                    <input name="points0" id="points0" type="number" min="0" class="form-control form-control-solid" placeholder="Enter points" />
+                                    <!--end::Input-->
+                                </div>
+                                <!--end::Col-->
+                            </div>
+                            <!--end::Input group-->
 
 
-    <div class="card px-30 ">
-        <div class="card-header">
+
+
+
+
+                        </div>
+                        <!--end::Scroll-->
+                    </div>
+                    <!--end::Modal body-->
+                    <!--begin::Modal footer-->
+                    <div class="modal-footer flex-center">
+                        <!--begin::Button-->
+                        <button type="reset" id="kt_modal_new_address_cancel" data-bs-toggle="modal" data-bs-target="#kt_modal_new_address" class="btn btn-light me-3">Discard</button>
+                        <!--end::Button-->
+                        <!--begin::Button-->
+                        <button type="submit" id="kt_modal_new_address_submit" class="btn btn-primary">
+                            <span class="indicator-label">Submit</span>
+                            <span class="indicator-progress">Please wait...
+                                <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                        </button>
+                        <!--end::Button-->
+                    </div>
+                    <!--end::Modal footer-->
+                </form>
+                <!--end::Form-->
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="mb-10">
+        <label for="exampleFormControlInput1" class="required form-label">Example</label>
+        <input type="email" class="form-control form-control-solid" placeholder="Example input" />
+    </div> -->
+
+
+    <div class="card mt-10 mx-19">
+        <div class="card-header  ">
             <h3 class="card-title"> <strong> Rules Table</strong></h3>
         </div>
-        <div class="card-body py-3">
-            <div class="table-responsive">
-                <table class="table table-row-dashed table-row-gray-300 align-middle gs-0  gy-4" id="rules_table">
+        <div class="card-body py-3 ">
+            <div class="table-responsive ">
+                <table class="table table-row-dashed table-row-gray-300 align-middle gs-0  gy-6" id="rules_table">
                     <thead>
                         <tr class="fw-bolder text-muted">
                             <th class="min-w-100px">#</th>
